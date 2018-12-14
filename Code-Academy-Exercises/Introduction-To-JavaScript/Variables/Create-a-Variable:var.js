dc99bd3ad9335f40e8dbf3d21ddfd2479a8af3a7
Samantha Morgan <samo5532@colorado.edu>
2018-12-14 08:22:02 -0700

    Finished the Variables section.

--- Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable:var.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable:var.js
@@ -1,6 +1,9 @@
 var favoriteFood = 'pizza';
+
 var numOfSlices = 8;
+
 console.log(favoriteFood);
+
 console.log(numOfSlices);
 
-/* On line one I used the keyword var, short for variable, to declare the variable "favoriteFood". I then assigned the value "pizza" to the variable "favoriteFood". On line 3 I called the variable to print its contents in the console, and "pizza" was printed. The best practice for naming variables is in camel casing, which makes the first word lowercase and everyword after that has its first letter capitalized. Variables cannot be named as keywords or begin with a number. */ 
+/* On line 1 I used the keyword var, short for variable, to declare the variable "favoriteFood". I then assigned the value "pizza" to the variable "favoriteFood". On line 5 I called the variable to print its contents in the console, and "pizza" was printed. The best practice for naming variables is in camel casing, which makes the first word lowercase and everyword after that has its first letter capitalized. Variables cannot be named as keywords or begin with a number. */ 
---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/The-Increment-and-Decrement-Operator.js
@@ -0,0 +1,12 @@
+let gainedDollar = 3;
+
+let lostDollar = 50;
+
+gainedDollar++;
+
+lostDollar--;
+
+
+
+
+/* On line 5 I used the increment operator "++" to add 1 to the value of the variable "gainedDollar". On line 7 I used the decrement operator "--" to subtract one from the "lostDollar" variable. As a result, the variable "gainedDollar" was updated and assigned a new value of 4, and the "lostDollar" variable was updated and assigned a new value of 49. */
