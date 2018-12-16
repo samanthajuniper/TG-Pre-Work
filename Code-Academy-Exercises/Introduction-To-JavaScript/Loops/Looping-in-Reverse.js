739691ec835d85997c43661be9ae6ba390e3a129
Samantha Morgan <samo5532@colorado.edu>
2018-12-16 13:14:09 -0700

    Finished Loops section.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Looping-in-Reverse.js
@@ -0,0 +1,9 @@
+// The loop below loops from 0 to 3. Edit it to loop backwards from 3 to 0
+for (let counter = 3; counter >= 0; counter--){
+  console.log(counter)
+};
+
+
+
+
+/* I modified my for loop to run backwards by setting the iterator variable to the highest number in my loop, setting the stopping codition greater than or equal to the lowest number, and telling the loop to subtract one from each iteration. Thus, the console printed "3,2,1,0" (see line 3). */
---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Repeating-Tasks-Manually.js
@@ -0,0 +1,9 @@
+// Write your code below
+
+let vacationSpots = ['Switzerland', 'Iceland', 'Hot Springs'];
+
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+/*In this example, each element of the "vacationSpots" array needs its own line of code if we want to log them separately to the console. This is an inefficent and tedious method that could be streamlined using For Loops. */
